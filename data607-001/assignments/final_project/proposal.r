50aa29cdbf098824c1a97f1bf2c482a9ee4117ce
Javier Pajuelo Bazan <quaere1verum@gmail.com>
2021-11-22 20:22:28 -0500

    Adding kaggle titanic data set

---
+++ data607-001/assignments/final_project/proposal.r
@@ -0,0 +1,25 @@
+#==============================================================================
+# Final Project Proposal
+# 
+# Data Science Project -Predicting survival on the Titanic
+#     Main Goal:  Create a model to predict survival on the Titanic.
+#     Motivation: I have been very interested in the titanic tragedy since I watched the James Cameron movie as a young boy.
+#                 Also, it seems interesting how the movie basically outlines the survival rate was largely derived by wealth.
+#                 I would like to investigate this using my newly acquired R knowledge.
+#     Data Sources: * Kaggle 
+#                   * https://www.encyclopedia-titanica.org/titanic-passenger-list/
+#                   * https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/problem12.html
+#     
+#
+#==============================================================================
+
+
+
+# Your project has a recognizable "data science workflow," such as the OSEMN workflow or Hadley Wickham's Grammar
+# of Data Science. [Example: First the data is acquired, then necessary transformations and clean-up are performed, then
+#                   the analysis and presentation work is performed]
+
+
+
+
+
